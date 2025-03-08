6f3c3dc4e405e6af7b78da34331418d06720da8f
Cliff Click <cliffc@acm.org>
2025-03-08 11:01:01 -0800

    Chapter 21: Instruction Encoding and ELF
    Co-authored-by: Tamás Hadházy <85063808+Hels15@users.noreply.github.com>
    Co-authored-by: Darrick Wiebe <darrickw@gmail.com>
    Co-authored-by: godalming123 <68993177+godalming123@users.noreply.github.com>
    Co-authored-by: Hadházy Tamás <helsgithub15@gmail.com>
    Co-authored-by: XmiliaH <45106915+XmiliaH@users.noreply.github.com>
    Co-authored-by: Dmitry Stogov <dmitrystogov@gmail.com>
    Co-authored-by: Yasser Arguelles Snape <yasserarguelles2506@gmail.com>

--- src/main/java/com/seaofnodes/simple/node/cpus/arm/LoadARM.java
+++ src/main/java/com/seaofnodes/simple/node/cpus/arm/LoadARM.java
@@ -1,12 +1,10 @@
 package com.seaofnodes.simple.node.cpus.arm;
 
-
 import com.seaofnodes.simple.SB;
-import com.seaofnodes.simple.codegen.CodeGen;
-import com.seaofnodes.simple.codegen.RegMask;
+import com.seaofnodes.simple.codegen.*;
 import com.seaofnodes.simple.node.LoadNode;
 import com.seaofnodes.simple.node.Node;
-
+import com.seaofnodes.simple.type.TypeFloat;
 
 // Load memory addressing on ARM
 // Support imm, reg(direct), or reg+off(indirect) addressing
@@ -14,24 +12,18 @@ import com.seaofnodes.simple.node.Node;
 // idx  = null
 // off  = off - offset added to base
 
-public class LoadARM extends MemOpARM{
-    LoadARM(LoadNode ld,Node base, Node idx, int off) {
+public class LoadARM extends MemOpARM {
+    LoadARM(LoadNode ld, Node base, Node idx, int off) {
         super(ld, base, idx, off, 0);
     }
-
-    @Override public RegMask regmap(int i) {
-        return arm.RMASK;
-    }
-
-    // Wide mask loads both ints and floats; encoding varies.
-    @Override public RegMask outregmap() { return arm.MEM_MASK; }
-
-
     @Override public String op() { return "ld"+_sz; }
-
+    @Override public RegMask outregmap() { return arm.MEM_MASK; }
+    // ldr(immediate - unsigned offset) | ldr(register)
+    @Override public void encoding( Encoding enc ) {
+        ldst_encode(enc, imm_op(), reg_op(), this, size());
+    }
     @Override public void asm(CodeGen code, SB sb) {
         sb.p(code.reg(this)).p(",");
         asm_address(code,sb);
     }
-
 }
